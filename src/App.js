7788c406f9ea4a02bde7f321676ab25bd8590d67
Tasha <tashingsworth@gmail.com>
2020-06-25 12:39:30 -0400

    consumed data with ProductContext, created CartContext

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
-import {ProductContext} from './contexts/ProductContext';
+import ProductContext from './contexts/ProductContext';
 
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 import Product from './components/Product';
+import CartContext from './contexts/CartContext';
 
 function App() {
 	const [products] = useState(data);
@@ -18,10 +19,16 @@ function App() {
 		setCart({...cart, item});
 	};
 
+	const removeItem = itemID => {
+		setCart(cart.filter(item => itemID !== item.id));
+	};
+
 	return (
 		<div className="App">
 			<Navigation cart={cart} />
 		<ProductContext.Provider value={{products, addItem }}>
+			<CartContext.Provider value = {{cart, removeItem}}>
+
 			{/* Routes */}
 			<Route exact path="/">
 				<Products />
@@ -30,7 +37,8 @@ function App() {
 			<Route path="/cart">
 				<ShoppingCart cart={cart} />
 			</Route>
-			</ProductContext.Provider>
+			</CartContext.Provider>
+		</ProductContext.Provider>
 		</div>
 	);
 }
--- src/contexts/ProductContext.js
+++ src/contexts/ProductContext.js
@@ -1,3 +1,6 @@
-import React, {createContext} from 'react';
+import {createContext} from 'react';
 
-export const ProductContext = createContext();
+const ProductContext = createContext();
+console.log(ProductContext);
+
+export default ProductContext;
