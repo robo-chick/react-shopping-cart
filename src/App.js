d191743e4c8bd17c10c668765c107184f106ce1f
Tasha <tashingsworth@gmail.com>
2020-06-25 12:24:45 -0400

    created ProductContext, imported to App.js and provided data.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
+import {ProductContext} from './contexts/ProductContext';
 
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
+import Product from './components/Product';
 
 function App() {
 	const [products] = useState(data);
@@ -19,15 +21,16 @@ function App() {
 	return (
 		<div className="App">
 			<Navigation cart={cart} />
-
+		<ProductContext.Provider value={{products, addItem }}>
 			{/* Routes */}
 			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
+				<Products />
 			</Route>
 
 			<Route path="/cart">
 				<ShoppingCart cart={cart} />
 			</Route>
+			</ProductContext.Provider>
 		</div>
 	);
 }
